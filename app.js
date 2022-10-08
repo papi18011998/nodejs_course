1c2e65d4d984cc6f5fcc46883096517df93754ba
papi18011998 <papaibrahima98@gmail.com>
2022-10-08 23:55:12 +0000

    return json of corresponding pokemon

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const pokemons = require('./api/mock-pokemon.js')
 
 const app = express()
 const port = 3000
@@ -6,6 +7,7 @@ app.get('/',(req,res)=> res.send('Hello Express'))
 
 app.get('/api/pokemons/:id',(req,res)=>{
     const id = req.params.id
-    res.send(`Vous avez demandé le pokémon n° ${id}`)
+    const pokemon = pokemons.find(pokemon => pokemon.id === parseInt(id))
+    res.json(pokemon)
 })
 app.listen(port,()=>console.log(`L'application tourne sur le port http://localhost:${port}`))
