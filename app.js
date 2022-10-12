e4cd5b11e234e85bafd8893c81232462b0bead75
papi18011998 <papaibrahima98@gmail.com>
2022-10-12 09:16:12 +0000

    connexion bdd, ajout de pokemon

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ const {success} = require('./helpers/helper.js')
 const bodyParser = require('body-parser')
 const morgan  = require('morgan')
 const { application } = require('express')
+const {Sequelize, DataTypes} = require('sequelize')
+const PokemonsModel = require('./src/models/pokemons')
+const Credentials = require('./helpers/credentials')
 const app = express()
 const port = 3000
 
+// create sequilze instance
+const sequelize = new Sequelize(
+    Credentials.dbname,
+    Credentials.username,
+    Credentials.password,
+    {
+        host: 'localhost',
+        dialect:'mysql'
+    }
+)
+sequelize.authenticate()
+.then(()=>console.log('connexion etablie'))
+.catch((error)=>console.log('erreur de connexion ',error))
+
+// mise en place de la synchronisation a la BDD
+
+const Pokemon = PokemonsModel(sequelize,DataTypes)
+sequelize.sync({alter: true})
+    .then(()=>console.log('La base est synchrone'))
 app
 .use(morgan('dev'))
 .use(bodyParser.json())
@@ -25,11 +47,15 @@ app.get('/api/pokemons/:id',(req,res)=>{
 })
 
 app.post('/api/pokemons',(req,res)=>{
-    const pokemonId = pokemons.length + 1
-    const pokemonCreated = {...req.body,...{id:pokemonId, createdAt: new Date()}} 
-    pokemons.push(pokemonCreated)
-    const message = 'Un pokemon a été créé.'
-    res.json(success(message,pokemonCreated))
+    const body = req.body
+    Pokemon.create({
+        name: body.name,
+        hp: body.hp,
+        cp: body.cp,
+        picture: body.picture,
+        types: body.types.join(),
+        createdAt: new Date()
+    }).then((response)=>res.json(response.toJSON()))    
 })
 
 app.put('/api/pokemons/:id',(req,res)=>{
